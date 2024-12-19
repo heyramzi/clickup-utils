619f4c133b7800e7e6d1023b322eb9a28fee98e9
heyramzi <ramzi@upsys-consulting.com>
2024-12-19 17:25:47 +0000

    Remove unused files: deleted .gitignore, package.json, README.md, tsconfig.json, and all TypeScript type definitions for ClickUp API.

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './clickup-types/clickup-api-types';
-export * from './clickup-types/clickup-hierarchy-types';
-export * from './clickup-types/clickup-task-types';
-export * from './clickup-types/clickup-field-types';
